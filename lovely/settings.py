c59ff693709ad3c7adcdee37af4429cbdc680a7e
Iman Gholami <imangholami77@gmail.com>
2024-05-21 18:00:52 -0400

    Fix time diff

--- lovely/settings.py
+++ lovely/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    BASE_DIR / 'fingerprint/static'
+    BASE_DIR / 'fingerprint/static',
+    BASE_DIR / 'billboard/static',
 ]
 
 MEDIA_URL = '/media/'

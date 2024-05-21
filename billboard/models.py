24a8cf7d804f718f8bc4b841a50f8dd3e5f34680
Iman Gholami <imangholami77@gmail.com>
2024-05-21 15:13:33 -0400

    Implement image

--- billboard/models.py
+++ billboard/models.py
@@ -14,6 +14,8 @@ class Relationship(models.Model):
 class Participation(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.SET_NULL, null=True)
     relationship = models.ForeignKey(Relationship, on_delete=models.SET_NULL, null=True, related_name='participants')
+    photo = models.ImageField(upload_to='photos', blank=True, null=True)
+    photo_position = models.CharField(max_length=5, choices=[('left', 'Left'), ('right', 'Right')], default='left')
 
     def __str__(self):
         return f"{self.user.username if self.user else 'Nobody'} in ({self.relationship})"
